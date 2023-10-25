438ebc0f2ec27277be8a24f0a78e6ce18be93b8d
GuilhermeCasagrandeF <guilherme.casagrande@soeltech.com.br>
2023-10-25 16:32:48 -0300

    Componente campo texto e alterado padrão de arquivo principal dos componentes

---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,13 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+  return (
+    <div className="campo-texto">
+      <label htmlFor="">Nome</label>
+      <input type="text" placeholder='Digite o seu nome'/>
+    </div>
+  )
+
+}
+
+export default CampoTexto;
