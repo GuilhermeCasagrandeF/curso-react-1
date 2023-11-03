2423eed7f0735a3d0cfff9ef686b17be0b319c9b
GuilhermeCasagrandeF <guilherme.casagrande@soeltech.com.br>
2023-11-03 10:07:31 -0300

    Passando estilos css como props

--- src/App.js
+++ src/App.js
@@ -4,27 +4,62 @@ import Formulario from './componentes/Formulario';
 import Time from './componentes/Time';
 
 function App() {
-
-  const [colaboradores, setColaboradores] = useState([]);
-
-  const aoNovoColaboradorCadastrado = (colaborador) => {
-    console.log(colaborador);
-    setColaboradores([...colaboradores, colaborador]);
-    console.log(colaboradores);
-  }
-
-  return (
-    <div className="App">
-
-      <Banner />
-      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorCadastrado(colaborador)} />
-
-      <Time nome="Programação" />
-      <Time nome="Front-end" />
-      <Time nome="Data Science" />
     
-    </div>
-  );
-}
+    const [colaboradores, setColaboradores] = useState([]);
+    
+    const times = [
+        {
+            nome: 'Programação',
+            corPrimaria: '#57C278',
+            corSecundaria: '#D9F7E9',
+        },
+        {
+            nome: 'Front-End',
+            corPrimaria: '#82CFFA',
+            corSecundaria: '#E8F8FF',
+        },
+        {
+            nome: 'Data Science',
+            corPrimaria: '#A6D157',
+            corSecundaria: '#F0F8E2',
+        },
+        {
+            nome: 'Devops',
+            corPrimaria: '#E06B69',
+            corSecundaria: '#FDE7E8',
+        },
+        {
+            nome: 'UX e Design',
+            corPrimaria: '#DB6EBF',
+            corSecundaria: '#FAE9F5',
+        },
+        {
+            nome: 'Mobile',
+            corPrimaria: '#FFBA05',
+            corSecundaria: '#FFF5D9',
+        },
+        {
+            nome: 'Gestão e Inovação',
+            corPrimaria: '#FF8A29',
+            corSecundaria: '#FFEEDF',
+        },
+    ];
+    
+    const aoNovoColaboradorCadastrado = (colaborador) => {
+        setColaboradores([...colaboradores, colaborador]);
+    }
+    
+    return (
+        <div className="App">
+            
+            <Banner />
+            <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorCadastrado(colaborador)} />
 
+            {times.map(time => <Time nome={time.nome} key={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria} />)}
+            
+        </div>
+    );
+}
+    
 export default App;
+    
